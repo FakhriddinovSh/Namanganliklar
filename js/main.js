5d1a333bc08b5358559c31eb38834437d53cd7cb
FakhriddinovSh <shaxobiddinfaxriddinov@yandex.com>
2022-05-20 10:30:36 +0500

    Nav added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// let search = document.querySelector(".search")
+
+// search.onclick = function(){
+//     document.body.classList.toggle("dark-mode")
+//     document.footer.id.toggle("dark-mode")
+// }
